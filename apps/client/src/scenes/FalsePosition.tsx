51ed4cddaac6f224a510a0b0ea9d12996cb5d418
Gusb3ll <kitpipat.0647@gmail.com>
2024-09-14 19:40:20 +0700

    feat: false position

--- apps/client/src/scenes/FalsePosition.tsx
+++ apps/client/src/scenes/FalsePosition.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from '@tanstack/react-query'
+import { createColumnHelper } from '@tanstack/react-table'
 import { MathJax } from 'better-react-mathjax'
 import dynamic from 'next/dynamic'
 import { useMemo, useState } from 'react'
@@ -7,7 +8,12 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { PuffLoader } from 'react-spinners'
 import { toast } from 'sonner'
 
-import { FalsePositionArgs, falsePosition } from '@/services/root'
+import { DataTable } from '@/components/DataTable'
+import {
+  FalsePositionArgs,
+  FalsePositionResponse,
+  falsePosition,
+} from '@/services/root'
 import { NotoSansMath } from '@/utils'
 
 type Series = {
@@ -19,7 +25,7 @@ const Chart = dynamic(() => import('react-charts').then(mod => mod.Chart), {
   ssr: false,
 }) as typeof ChartType
 
-const FalsePosition: React.FC = () => {
+const FalsePositionScene: React.FC = () => {
   const [func, setFunc] = useState<string>('')
   const [data, setData] = useState<Series[]>([
     {
@@ -84,77 +90,116 @@ const FalsePosition: React.FC = () => {
     [],
   )
 
+  const columnHelper = createColumnHelper<FalsePositionResponse[0]>()
+  const columns = [
+    columnHelper.accessor('i', {
+      cell: info => info.getValue(),
+      header: 'Iteration',
+    }),
+    columnHelper.accessor('xl', {
+      cell: info => info.getValue(),
+      header: (<MathJax>{'`X_L`'}</MathJax>) as unknown as string,
+    }),
+    columnHelper.accessor('xr', {
+      cell: info => info.getValue(),
+      header: (<MathJax>{'`X_R`'}</MathJax>) as unknown as string,
+    }),
+    columnHelper.accessor('xm', {
+      cell: info => info.getValue(),
+      header: (<MathJax>{'`X_m`'}</MathJax>) as unknown as string,
+    }),
+    columnHelper.accessor('error', {
+      cell: info => info.getValue(),
+      header: 'Error',
+    }),
+  ]
+
   return (
-    <div className="box-shadow-example mx-auto my-4 flex h-[90dvh] w-[97dvw] flex-grow flex-row justify-between rounded-[12px] p-4">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-2 p-4">
-          <div className="flex flex-row items-center gap-1">
-            <MathJax>{'`f(x) = `'}</MathJax>
-            <input
-              required
-              type="text"
-              className={`border p-2 ${NotoSansMath.className}`}
-              onChange={e => {
-                setFunc(e.currentTarget.value)
-                setValue('func', e.currentTarget.value)
-              }}
-            />
-          </div>
-          <div className="py-4">
-            <MathJax>{'`f(x) = $`'.replaceAll('$', func ? func : '')}</MathJax>
-          </div>
-          <div className="ml-3 flex flex-row items-center gap-1">
-            <MathJax>{'`X_L = `'}</MathJax>
-            <input
-              type="number"
-              step=".000001"
-              className={`border p-2 ${NotoSansMath.className}`}
-              {...register('xl', { required: true })}
-            />
-          </div>
-          <div className="ml-3 flex flex-row items-center gap-1">
-            <MathJax>{'`X_R = `'}</MathJax>
-            <input
-              type="number"
-              step=".000001"
-              className={`border p-2 ${NotoSansMath.className}`}
-              {...register('xr', { required: true })}
-            />
-          </div>
-          <div className="ml-1.5 flex flex-row items-center gap-1">
-            <MathJax>{'`Err = `'}</MathJax>
-            <input
-              type="number"
-              step=".000001"
-              className={`border p-2 ${NotoSansMath.className}`}
-              defaultValue="0.00001"
-              {...register('error', { required: true })}
-            />
-          </div>
-        </div>
-        <button
-          disabled={isSubmitting}
-          type="submit"
-          className={`mt-2 flex w-full items-center justify-center rounded-lg bg-gray-400 py-2 text-white transition-all hover:bg-gray-500 active:bg-gray-600 ${isSubmitting ? 'cursor-not-allowed' : ''}`}
+    <div className="box-shadow-example m-8 my-4 rounded-[12px] p-4">
+      <div className="grid h-[40dvh] grid-flow-row grid-cols-2 place-content-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center"
         >
-          {isSubmitting ? (
-            <PuffLoader size={24} color="#FFFFFF" />
-          ) : (
-            'Calculate'
-          )}
-        </button>
-      </form>
-      <div className="h-[60%] w-[60%]">
-        <Chart
-          options={{
-            data,
-            primaryAxis,
-            secondaryAxes,
-          }}
-        />
+          <div className="flex flex-col gap-2 p-4">
+            <div className="flex flex-row items-center gap-1">
+              <MathJax>{'`f(x) = `'}</MathJax>
+              <input
+                required
+                type="text"
+                className={`border p-2 ${NotoSansMath.className}`}
+                onChange={e => {
+                  setFunc(e.currentTarget.value)
+                  setValue('func', e.currentTarget.value)
+                }}
+              />
+            </div>
+            <div className="py-4">
+              <MathJax>
+                {'`f(x) = $`'.replaceAll('$', func ? func : '')}
+              </MathJax>
+            </div>
+            <div className="ml-3 flex flex-row items-center gap-1">
+              <MathJax>{'`X_L = `'}</MathJax>
+              <input
+                type="number"
+                step=".000001"
+                className={`border p-2 ${NotoSansMath.className}`}
+                {...register('xl', { required: true })}
+              />
+            </div>
+            <div className="ml-3 flex flex-row items-center gap-1">
+              <MathJax>{'`X_R = `'}</MathJax>
+              <input
+                type="number"
+                step=".000001"
+                className={`border p-2 ${NotoSansMath.className}`}
+                {...register('xr', { required: true })}
+              />
+            </div>
+            <div className="ml-1.5 flex flex-row items-center gap-1">
+              <MathJax>{'`Err = `'}</MathJax>
+              <input
+                type="number"
+                step=".000001"
+                className={`border p-2 ${NotoSansMath.className}`}
+                defaultValue="0.00001"
+                {...register('error', { required: true })}
+              />
+            </div>
+          </div>
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className={`mt-2 flex items-center justify-center rounded-lg bg-gray-400 px-24 py-2 text-white transition-all hover:bg-gray-500 active:bg-gray-600 ${isSubmitting ? 'cursor-not-allowed' : ''}`}
+          >
+            {isSubmitting ? (
+              <PuffLoader size={24} color="#FFFFFF" />
+            ) : (
+              'Calculate'
+            )}
+          </button>
+        </form>
+        <div className="h-full w-full">
+          <Chart
+            options={{
+              data,
+              primaryAxis,
+              secondaryAxes,
+            }}
+          />
+        </div>
       </div>
+
+      {falsePositionMutation.data ? (
+        <div className="col-span-2 mt-8 h-full w-full">
+          <DataTable columns={columns} data={falsePositionMutation.data} />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
 
-export default FalsePosition
+export default FalsePositionScene
